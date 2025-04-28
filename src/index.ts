13bb4bce3266f4b3dee3d4ad287b2a99daa956e8
Krystian Mikołajczyk <krystianb127098@outlook.com>
2025-04-28 11:44:26 +0200

    Add initial project files and configuration for Rust and TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world")
+// console.log("hello world")
+
+import iohook from "iohook";
+
+iohook.registerShortcut(["ctrl+alt+shift+s"], () => {
+  console.log("shortcut pressed");
+});
+
+iohook.start();
